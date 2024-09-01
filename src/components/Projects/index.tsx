45a43337da5d5070b1e34e8ef50190f8d800160f
Israel Nazareth <israelsnazareth@gmail.com>
2024-09-01 13:07:32 -0300

    fix: redirect button

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -2,33 +2,37 @@ import projectsData from "./projectsData.json";
 import "./styles.css";
 
 export default function Projects() {
+  const redirectTo = (url: string) => window.open(url, "_blank");
+
   return (
     <div data-aos="fade-in" className="projects" id="projects">
       <h1 className="title">Projetos</h1>
       <div className="projects-container">
-        {projectsData.map(({ img, title, description, pageURL, githubURL }) => (
-          <div data-aos="fade-in" className="project" key={title}>
-            <div className="project-content">
-              <a href={pageURL} target="blank">
-                <img src={img} alt={title} width="100%" height="auto" />
-              </a>
-              <h1 className="project-title">{title}</h1>
-              <p className="project-description">{description}</p>
-            </div>
-            <div className="buttons-container">
-              <button className="button-left">
-                <a href={pageURL} target="blank">
+        {projectsData.map(
+          ({ img, title, description, deployUrl, githubURL }) => (
+            <div data-aos="fade-in" className="project" key={title}>
+              <div className="project-content">
+                <a href={deployUrl} target="blank">
+                  <img src={img} alt={title} width="100%" height="auto" />
+                </a>
+                <h1 className="project-title">{title}</h1>
+                <p className="project-description">{description}</p>
+              </div>
+              <div className="buttons-container">
+                <button
+                  onClick={() => redirectTo(deployUrl)}
+                  disabled={!deployUrl}
+                  title={!deployUrl ? "Deploy em desenvolvimento" : ""}
+                >
                   Ver aplicação
-                </a>
-              </button>
-              <button className="button-right">
-                <a href={githubURL} target="blank">
+                </button>
+                <button onClick={() => redirectTo(githubURL)}>
                   Ver código
-                </a>
-              </button>
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
