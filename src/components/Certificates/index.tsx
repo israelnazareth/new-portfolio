1ca76ebdb2e3ebbc5fce07ee40ce6e01bc02dd67
Israel Nazareth <israelsnazareth@gmail.com>
2024-09-03 22:56:50 -0300

    chore: react-slick for certificates carousel

--- src/components/Certificates/index.tsx
+++ src/components/Certificates/index.tsx
@@ -1,20 +1,60 @@
+import Slider, { Settings } from "react-slick";
 import certificatesData from "./certificatesData.json";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./styles.css";
 
 export default function Certificates() {
+  const settings: Settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "0",
+    dots: true,
+    slidesToShow: 3,
+    speed: 500,
+    initialSlide: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="certificates" id="certificates">
+    <div data-aos="fade-in" className="certificates" id="certificates">
       <h1 className="title">Certificados</h1>
-      <div className="certificates-container">
-        {certificatesData.map(({ img, path, title }) => (
-          <div data-aos="fade-in" className="certificate" key={title}>
-            <div className="certificate-content">
+      <div className="slider-container">
+        <Slider {...settings}>
+          {certificatesData.map(({ img, path, title }) => (
+            <div data-aos="fade-in" className="certificate" key={title}>
               <a href={path} target="blank">
                 <img src={img} alt={title} loading="lazy" title={title} />
               </a>
             </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
     </div>
   );
