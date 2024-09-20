97bcd78f755c2ee1b2847fd23d609ed7098d8b6f
Israel Nazareth <israelsnazareth@gmail.com>
2024-09-20 00:23:23 -0300

    feat: colorful technology icons

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -8,33 +8,33 @@ export default function Techs() {
       <h1 className="title">Tecnologias</h1>
       <div data-aos="fade-in" className="techs-container">
         <div className="techs-data">
-          <IconFA.FaReact />
-          <IconSI.SiRedux />
+          <IconFA.FaReact color="#5ED3F3" />
+          <IconSI.SiRedux color="#7A4FBE" />
           <h1 className="techs-name">React e Redux</h1>
         </div>
         <div className="techs-data">
-          <IconFA.FaHtml5 />
-          <IconFA.FaCss3Alt />
+          <IconFA.FaHtml5 color="#F26A2F" />
+          <IconFA.FaCss3Alt color="#3F9DD7" />
           <h1 className="techs-name">HTML5 e CSS3</h1>
         </div>
         <div className="techs-data">
-          <IconSI.SiJavascript className="logoJs" />
-          <IconSI.SiTypescript />
+          <IconSI.SiJavascript className="logoJs" color="#F7E028" />
+          <IconSI.SiTypescript color="#167ECF" />
           <h1 className="techs-name">Javascript e Typescript</h1>
         </div>
         <div className="techs-data">
-          <IconFA.FaGitAlt />
+          <IconFA.FaGitAlt color="#F15539" />
           <IconFA.FaGithub />
           <h1 className="techs-name">Git e GitHub</h1>
         </div>
         <div className="techs-data">
-          <IconFA.FaNpm />
-          <IconFA.FaNodeJs />
+          <IconFA.FaNpm color="#CC3F3D" />
+          <IconFA.FaNodeJs color="#689F63" />
           <h1 className="techs-name">Npm e Node.js</h1>
         </div>
         <div className="techs-data">
-          <IconSI.SiMysql />
-          <IconSI.SiMongodb />
+          <IconSI.SiMysql color="#05718A" />
+          <IconSI.SiMongodb color="#54AD47" />
           <h1 className="techs-name">MySQL e MongoDB</h1>
         </div>
       </div>
