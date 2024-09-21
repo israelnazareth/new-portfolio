633031a23c3389b0d1ef33c64db55650c0d75f65
Israel Nazareth <israelsnazareth@gmail.com>
2024-09-21 00:56:12 -0300

    feat: create animation hook

---
+++ src/hooks/useAnimationOnScroll.ts
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from 'react';
+
+type UseFadeInOnScrollReturn = {
+  refs: React.RefObject<HTMLDivElement>[];
+  ref: React.RefObject<HTMLDivElement>;
+};
+
+type Animation = 'fadeIn';
+
+const useAnimationOnScroll = (animation: Animation = 'fadeIn', arraySize: number = 1): UseFadeInOnScrollReturn => {
+  const array = Array.from({ length: arraySize }, () => React.createRef<HTMLDivElement>());
+
+  const refs = useRef<React.RefObject<HTMLDivElement>[]>(array).current;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      refs.forEach((ref) => {
+        const element = ref.current;
+
+        if (element) {
+          const { top, bottom } = element.getBoundingClientRect();
+          const windowHeight = window.innerHeight;
+
+          if (top <= windowHeight && bottom >= 0) {
+            return element.classList.add(animation);
+          }
+
+          element.classList.remove(animation);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [refs, animation]);
+
+  return { refs, ref: refs[0] };
+};
+
+export default useAnimationOnScroll;
